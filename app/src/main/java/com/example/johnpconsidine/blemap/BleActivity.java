68b193a63b7334f8e1fa09f53803f78b5b3ff0c3
John P Considine <johnpconsidine@johns-mbp-2.dhcp.nd.edu>
2016-04-06 11:33:00 -0400

    before ble

---
+++ app/src/main/java/com/example/johnpconsidine/blemap/BleActivity.java
@@ -0,0 +1,18 @@
+package com.example.johnpconsidine.blemap;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class BleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ble);
+
+
+
+
+
+    }
+}
